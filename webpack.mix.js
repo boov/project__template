2a629cee3faedfd53b28dd1be01459563b07a4f6
Matthew Booth <matthew@boov.co.uk>
2020-01-08 22:26:41 +0000

    UPDATED: npm packages
    ADDED: l_grid layout component
    UPDATED: Removed variables from _flex and _grid

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,5 @@ const mix = require("laravel-mix");
 mix
   .disableNotifications()
   .setPublicPath("public")
-  .copy("assets/fonts/", "public/fonts")
-  .copy("assets/images/", "public/images")
-  .sass("assets/styles/main.scss", "public/styles")
-  .js("assets/scripts/main.js", "public/scripts/main.js")
-  .babel("public/scripts/main.js", "public/scripts/main.es5.js");
+  .sass("resources/styles/main.scss", "public/styles")
+  .js("resources/scripts/main.js", "public/scripts");
