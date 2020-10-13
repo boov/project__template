a63cb8dae33d829a17d18e31ce7f6412570e5a34
Matthew Booth <matthew@boov.co.uk>
2020-10-13 12:21:00 +0100

    ADDED: .sassrc file
    UPDATED: .gitignore and parcel.js config

--- parceljs.js
+++ parceljs.js
@@ -1,17 +1,18 @@
 const Parcel = require("parcel-bundler");
 
-const files = ["./resources/scripts/main.js", "./resources/styles/main.scss"];
+const files = [
+  "./resources/fonts/**/*",
+  "./resources/scripts/main.js",
+  "./resources/styles/main.scss"
+];
 
 const options = {
-  autoInstall: true,
-  cache: false,
   hmr: false,
   outDir: "./public",
-  scopeHoist: false,
   sourceMaps: false
 };
 
-(async function() {
+(async function () {
   const bundler = new Parcel(files, options);
   await bundler.bundle();
 })();
